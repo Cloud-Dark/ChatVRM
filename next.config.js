d2114b70b39e74ef8424ad32f62f0b6f2a72d455
Cloud Dark <clouddark1798@gmail.com>
2026-04-23 15:06:48 +0700

    ci: add GitHub Pages deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.BASE_PATH || "",
-  basePath: process.env.BASE_PATH || "",
+  assetPrefix: process.env.BASE_PATH || "/ChatVRM/",
+  basePath: process.env.BASE_PATH || "/ChatVRM",
   trailingSlash: true,
   publicRuntimeConfig: {
-    root: process.env.BASE_PATH || "",
+    root: process.env.BASE_PATH || "/ChatVRM",
   },
+  output: "export",
   images: {
+    unoptimized: true,
     domains: ["vroid-hub.pximg.net"],
   },
 };
